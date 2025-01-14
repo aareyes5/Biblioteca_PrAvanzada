41727bb9f497169714d023e9169010b58e901c8a
aareyes5 <aareyes5@espe.edu.ec>
2025-01-13 23:07:12 -0500

    ahoras si

---
+++ src/main/java/biblioteca/azure_basic_app/Configurations/CorsConfig.java
@@ -0,0 +1,25 @@
+package Conection.Mongo.Conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todos los endpoints
+                        .allowedOrigins("*") // Permitir todos los orígenes
+                        .allowedMethods("GET", "POST", "OPTIONS", "PUT") // Métodos permitidos
+                        .allowedHeaders("*"); // Permitir todos los encabezados
+                        
+            }
+        };
+    }
+}
